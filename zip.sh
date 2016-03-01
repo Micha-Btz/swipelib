41167e7136a39f5066796c25e4bee7de4da07b7a
Micha-Btz <github@mdomann.tk>
2016-03-01 18:25:36 +0100

    change zip dir

--- zip.sh
+++ zip.sh
@@ -6,7 +6,7 @@ NAME=($FILE-$CM_Version-$VERSION)
 
 echo "Making Zip"
 # zipping
-zip -r $NAME.zip META-INF lib
+zip -r $NAME.zip META-INF app
 echo "ZIP is Ready"
 echo "Signing ZIP"
 java -jar signapk.jar testkey.x509.pem testkey.pk8 $NAME.zip $NAME-SIGNED.zip
